680c6837b90dafab382037e7d10c4da811b27a95
CareyWong <xidiancc@gmail.com>
2020-02-02 20:50:14 +0800

    更新docker-compose版本

--- bash/base.sh
+++ bash/base.sh
@@ -40,6 +40,6 @@ sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # 安装 docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
