0bf68858ea15aef42128ac7b48b756963bcfe26e
CareyWong <careywong@pm.me>
2021-01-21 20:08:42 +0800

    Upgrade nodejs

--- bash/nodejs.sh
+++ bash/nodejs.sh
@@ -6,14 +6,12 @@
 sudo apt update
 
 # 安装 nodejs
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y nodejs
 # npm config set registry https://registry.npm.taobao.org
 
 # 安装 yarn
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install yarn -y
+npm install -g yarn 
 
 # pm2
 yarn global add pm2
