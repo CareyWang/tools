e2df71ef849658239a14c2b6db5315e482cb6666
Carey Wong <33325726+CareyWang@users.noreply.github.com>
2022-12-07 18:02:07 +0800

    Update php74.sh

--- bash/php74.sh
+++ bash/php74.sh
@@ -2,6 +2,7 @@
 # curl -fsSL https://git.io/JIRlS | bash
 # https://cdn.jsdelivr.net/gh/CareyWang/tools@master/bash/php74.sh
 
+# add-apt-repository ppa:ondrej/php -y
 apt update
 apt install -y php7.4 php7.4-fpm php7.4-mysql php7.4-curl php7.4-xml php7.4-zip php7.4-gd php7.4-mbstring php7.4-json php7.4-redis php7.4-bcmath -y
 
